b6f8b9243130b54c6130f7c9b185066bc16b00f2
Konrad Borowski <konrad@borowski.pw>
2019-01-10 15:14:29 +0100

    Use BinaryHeap from comparator crate

--- examples/roomlist.rs
+++ examples/roomlist.rs
@@ -1,7 +1,7 @@
+use comparator::collections::BinaryHeap;
+use comparator::{comparing, Comparator};
 use pokemon_showdown_client::message::{Kind, QueryResponse, Room};
 use pokemon_showdown_client::{Result, Showdown};
-use std::cmp::Ordering;
-use std::collections::BinaryHeap;
 
 fn main() -> Result<()> {
     let mut connection = Showdown::connect("play.pokemonshowdown.com")?;
@@ -10,12 +10,13 @@ fn main() -> Result<()> {
         if let Kind::QueryResponse(QueryResponse::Rooms(rooms)) = connection.receive()?.parse().kind
         {
             println!("Top 5 most popular rooms");
-            let mut rooms: BinaryHeap<_> = rooms
-                .iter()
-                .map(|r| ComparingWith(|r: &&Room| r.user_count, r))
-                .collect();
+            let mut rooms_heap = BinaryHeap::with_comparator(
+                comparing(|r: &&Room| r.user_count)
+                    .then_comparing(comparing(|r: &&Room| &r.title).reversed()),
+            );
+            rooms_heap.extend(rooms.iter());
             for _ in 0..5 {
-                if let Some(ComparingWith(_, room)) = rooms.pop() {
+                if let Some(room) = rooms_heap.pop() {
                     println!("{} with {} users", room.title, room.user_count);
                 } else {
                     break;
@@ -23,44 +24,5 @@ fn main() -> Result<()> {
             }
             return Ok(());
         }
-    }
-}
-
-struct ComparingWith<F, T>(F, T);
-
-impl<F, T, U> PartialEq for ComparingWith<F, T>
-where
-    F: Fn(&T) -> U,
-    U: PartialEq,
-{
-    fn eq(&self, other: &Self) -> bool {
-        (self.0)(&self.1) == (other.0)(&other.1)
-    }
-}
-
-impl<F, T, U> Eq for ComparingWith<F, T>
-where
-    F: Fn(&T) -> U,
-    U: Eq,
-{
-}
-
-impl<F, T, U> PartialOrd for ComparingWith<F, T>
-where
-    F: Fn(&T) -> U,
-    U: PartialOrd,
-{
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        (self.0)(&self.1).partial_cmp(&(other.0)(&other.1))
-    }
-}
-
-impl<F, T, U> Ord for ComparingWith<F, T>
-where
-    F: Fn(&T) -> U,
-    U: Ord,
-{
-    fn cmp(&self, other: &Self) -> Ordering {
-        (self.0)(&self.1).cmp(&(other.0)(&other.1))
     }
 }
