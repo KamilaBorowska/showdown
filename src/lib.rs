d9f91d10fb863ae26fbff900f878e08e9acda749
Konrad Borowski <konrad@borowski.pw>
2019-02-13 10:23:58 +0100

    This crate doesn't use nightly features anymore

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,10 @@
 //!
 //! This crate is not stable, not even close. It requires nightly to be usable
 //! in practice as it's impossible to deal with lifetime errors related to
-//! self-borrows without async blocks, and uses features that are very likely to
-//! change. Additionally, the APIs of this crate are heavily experimented on,
-//! and there isn't going to be depreciation period for removed features. Don't
-//! use this crate if you aren't prepared for constant breakage.
+//! self-borrows without async blocks. Additionally, the APIs of this crate are
+//! heavily experimented on, and there isn't going to be depreciation period for
+//! removed features. Don't use this crate if you aren't prepared for constant
+//! breakage.
 
 pub mod message;
 
